e81f0c7aec7981b2ebfe19dff3cb8ce0ddb328a8
Marko <159923573+Markuut@users.noreply.github.com>
2024-11-21 17:21:10 +0200

    Add solution to 03 reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const reverseArray = [];
 
-};
+    for (let i = 0; i <= string.length; i++) {
+        reverseArray[i] = string.charAt(string.length - i);
+    }
+let reversedString = reverseArray.join("");
+
+return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
