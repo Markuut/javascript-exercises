28e180f8c740d359622a4b965a85b770786e6a2e
Marko <159923573+Markuut@users.noreply.github.com>
2024-11-21 18:31:29 +0200

    Add solution to 04 removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+   // return array.filter(item => !theArgs.includes(item)); // using .filter array function
+    let resultArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!theArgs.includes(array[i])) {
+            resultArray.push(array[i]);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
