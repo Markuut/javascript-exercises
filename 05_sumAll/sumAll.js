13138d7cbe13fb21800f91f447cfe6753c72a385
Marko <159923573+Markuut@users.noreply.github.com>
2024-11-28 15:15:20 +0200

    Add solution to 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+ 
+    if (!Number.isInteger(firstNumber) || firstNumber < 0 || !Number.isInteger(lastNumber) || lastNumber < 0) return "ERROR";
+    if (firstNumber > lastNumber) [firstNumber, lastNumber] = [lastNumber, firstNumber];
+        
+    let sumOfNumbers = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        sumOfNumbers = sumOfNumbers + i;
+    }
 
+return sumOfNumbers;
 };
 
 // Do not edit below this line
